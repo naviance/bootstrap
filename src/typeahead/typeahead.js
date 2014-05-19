e38ef21dc54f0dead44c4cddcfe6f39cdd3d074f
Vikas Garg <vikas.garg@hobsons.com>
2014-05-19 15:24:19 -0400

    Fix jshint warnings and update object definition.

--- src/typeahead/typeahead.js
+++ src/typeahead/typeahead.js
@@ -199,7 +199,7 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
       //$parsers kick-in on all the changes coming from the view as well as manually triggered by $setViewValue
       modelCtrl.$parsers.unshift(function (inputValue) {
 
-        scope.focus = { hasFocus = true };
+        scope.focus = { hasFocus: true };
 
         if (inputValue && inputValue.length >= minSearch) {
           if (waitTime > 0) {
@@ -370,7 +370,7 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
 
         scope.hasActive = function () {
             return scope.active != -1;
-        }
+        };
 
         scope.isActive = function (matchIdx) {
           return scope.active == matchIdx;
@@ -386,7 +386,7 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
 
         scope.hasSelected = function () {
             return !!scope.selected;
-        }
+        };
 
         scope.showNoMatch = function () {
             // The no match message should be shown if..
@@ -397,7 +397,7 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
             //   b. The query has been modified since the selection
             // 4. Field has focus
             return scope.query && !scope.isOpen() && !scope.hasSelected() && scope.focus.hasFocus;
-        }
+        };
       }
     };
   })
